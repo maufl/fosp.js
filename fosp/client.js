e03a10d23c4acc12f98c64540fc1ac2519721de5
Felix Maurer <Felix.Maurer@gmx.net>
2013-08-20 17:15:49 +0200

    Test the universal module approach on client.js

--- fosp/client.js
+++ fosp/client.js
@@ -1,24 +1,32 @@
 // fosp client object protoype
-var events = require('events');
-var WebSocket = require('websocket');
-var Message = require('./message');
-var Connection = require('./connection');
+(function(){
+var buildModule = function(events, WebSocket, Message, Connection) {
+  var Client = function(options) {
+    var self = this;
+    if (typeof options !== 'object' || options === null)
+      options = {}
+    self.port = options.port || 1337;
+    self.host = options.host || 'localhost.localdomain';
+    self.con = null
 
-var Client = function(options) {
-  var self = this;
-  if (typeof options !== 'object' || options === null)
-    options = {}
-  self.port = options.port || 1337;
-  self.host = options.host || 'localhost.localdomain';
-  self.con = null
+    self.wsc = new WebSocket.client()
+    self.wsc.connect('ws://' + self.host + ':' + self.port);
+    self.wsc.on('connect', function(connection) {
+      self.con = new Connection(connection);
+      self.emit('connect')
+    })
+  };
+  Client.prototype = Object.create(events.EventEmitter.prototype);
 
-  self.wsc = new WebSocket.client()
-  self.wsc.connect('ws://' + self.host + ':' + self.port);
-  self.wsc.on('connect', function(connection) {
-    self.con = new Connection(connection);
-    self.emit('connect')
+  return Client
+}
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = buildModule(require('events'), require('websocket'), require('./message'), require('./connection'));
+}
+else if (typeof define === 'function' && define.amd) {
+  define(['events','./message','./connection'], function(events, Message, Connection) {
+    return buildModule(events, WebSocket, Message, Connection);
   })
-};
-Client.prototype = Object.create(events.EventEmitter.prototype);
-
-module.exports = Client;
+}
+})();
